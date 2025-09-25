06cce01faf71c5db1fd825ba2248ae3737d8b581
Jack Wong <jackuli1998@gmail.com>
2025-09-25 11:38:38 +0800

    Implement get_number_only function in main()

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,19 @@
 #include "Book.h"
 using namespace std;
 
+int get_number_only() //This is a input validate function for number only (Jack)
+{
+    int number;
+
+    cout << "Enter a number: ";
+    if (cin >> number){
+        return number;
+    }
+    else{
+        return 0;
+    }
+}
+
 int main() {
     string name, author, content;
     int choice;
